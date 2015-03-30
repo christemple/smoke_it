57dced3d87193881d2bc115af49ea37cd425143c
Chris Temple and Tom Kadwill <christemple+tomkadwill@notonthehighstreet.com>
2015-03-30 18:03:47 +0100

    Fixing broken test

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -10,9 +10,9 @@ end
 
 When(/^search for 'cats'$/) do
   $browser.text_field(name: 'term').set('cats')
-  $browser.submit(:class, 'submit_search').click
+  $browser.button(:class, 'submit_search').click
 end
 
 Then(/^I should see the search results page for 'cats'$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect($browser.text).to include('YOU SEARCHED FOR "CATS"')
 end
